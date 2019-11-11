b2eca5585bc3764c1748bb82951aad5b64a8529a
Erica McConnaughey <ericamcconnaughey@gmail.com>
2019-11-10 22:40:50 -0600

    add functions to create string with middle character, input sentence, and last and first letters capitalized.

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,30 @@ var inputSentence = prompt("Enter a sentence:")
 
 console.log(inputSentence);
 
-var firstAndLast = function(firstLetter, lastLetter) {
-  return(firstLetter.toUpperCase());
-  return(lastLetter.toUpperCase());
-}
-
 var firstLetterInput = inputSentence.charAt(0);
 var lastLetterInput = inputSentence.charAt(inputSentence.length-1);
 
-console.log(firstAndLast(firstLetterInput + lastLetterInput));
-
 var reverseLetters = function(firstLetter, lastLetter) {
-  return(lastLetter.toUpperCase() + firstLetter.toUpperCase());
+  return lastLetter.toUpperCase() + firstLetter.toUpperCase();
 }
 
 console.log(reverseLetters(firstLetterInput, lastLetterInput));
 
-var runBothFunctions = function(firstLetter, lastLetter) {
-  console.log(firstAndLast(firstLetter, lastLetter));
-  alert(inputSentence + reverseLetters(firstLetter, lastLetter));
+// var runBothFunctions = function(firstLetter, lastLetter) {
+//   alert(inputSentence + reverseLetters(firstLetter, lastLetter));
+// }
 
+// runBothFunctions(firstLetterInput, lastLetterInput);
+
+var getMiddleCharacter = function(sentence) {
+  return sentence.charAt(sentence.length / 2);
 }
 
-runBothFunctions(firstLetterInput, lastLetterInput);
+console.log(getMiddleCharacter(inputSentence));
+
+var middleCharacter = getMiddleCharacter(inputSentence); 
+var reversedLetters = reverseLetters(firstLetterInput, lastLetterInput);
+
+var message = middleCharacter + inputSentence + reversedLetters;
+
+alert(message);
